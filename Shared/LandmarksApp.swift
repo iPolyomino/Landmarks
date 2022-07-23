4c76782732c925eb424de485a70d2bc217638b4b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-23 22:57:15 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 萩倉丈 on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
